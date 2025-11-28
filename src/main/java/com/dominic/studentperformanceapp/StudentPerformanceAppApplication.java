51b5dbf7041556e5fa9baa79120645c3165cf4c7
domeb <domebingonye@gmail.com>
2025-11-28 16:30:31 +0100

    Initial commit

---
+++ src/main/java/com/dominic/studentperformanceapp/StudentPerformanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.dominic.studentperformanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPerformanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominic/studentperformanceapp/StudentPerformanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominic.studentperformanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
